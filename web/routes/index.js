1150e445595b46bc236be84bb492770dae4d252f
Louis Pilfold <louis@lpil.uk>
2016-06-26 19:13:56 +0100

    Use static middleware to serve client

--- web/routes/index.js
+++ web/routes/index.js
@@ -9,17 +9,15 @@ const app = express();
 (process.env.NODE_ENV !== 'test') && app.use(morgan('dev'))
 
 app.disable('etag');
-app.use(express.static('vendor'));
-app.use(bodyParser.json());
 app.use(cookieSession({ keys: ["Hack me, I don't care."] }));
 app.use(tagUserMiddleware);
+app.use(express.static('client'));
+app.use(express.static('vendor'));
+app.use(bodyParser.json());
 
-const page          = require('./page');
 const controlSchema = require('./control-schema');
 const control       = require('./control');
 
-app.get('/', page.home);
-app.get('/main.js', page.js);
 app.post('/control-schema', controlSchema.create);
 app.get('/control-schema', controlSchema.show);
 app.post('/controls', control.create);
